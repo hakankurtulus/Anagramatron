a60ee8c42b15d06d00a016c1e888565496e55c18
Hakan Kurtulus <hakankurtulus@conatusoft.com>
2016-03-14 20:43:53 +0000

    test

---
+++ java/src/test/java/com/conatusoft/interview/anagramatron/test/AnagramatronTest.java
@@ -0,0 +1,73 @@
+package com.conatusoft.interview.anagramatron.test;
+
+import com.conatusoft.interview.anagramatron.Anagramatron;
+import com.conatusoft.interview.anagramatron.AnagramatronImpl;
+import com.conatusoft.interview.anagramatron.DictionaryService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by hakank on 13/03/2016.
+ */
+public class AnagramatronTest {
+
+    DictionaryService mockedDictionaryService;
+
+
+    private List<String> readWordsListFromFile(String fileName) {
+        Scanner scanner = null;
+        String data = "";
+        try {
+            scanner = new Scanner(Paths.get(fileName), StandardCharsets.UTF_8.name());
+            data = scanner.useDelimiter("\\A").next();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            scanner.close();
+        }
+
+        return Arrays.asList(data.split("\n"));
+    }
+
+    @Before
+    public void setUp() {
+        mockedDictionaryService = mock(DictionaryService.class);
+    }
+
+
+    @Test
+    public void testRetieveMaxAnagramSet() {
+        when(mockedDictionaryService.retrieveWordList()).thenReturn(
+                Arrays.asList("Miles", "dog", "peek", "dummy", "keep", "Limes", "god", "skating", "takings", "Smile", "Slime", "dummy", "tasking"));
+
+        Anagramatron anagramatron = new AnagramatronImpl(mockedDictionaryService);
+
+        List<String> maxAnagramSet = anagramatron.retrieveMaxAnagramSet();
+        System.out.println(maxAnagramSet);
+        assertEquals(maxAnagramSet.size(), 4);
+    }
+
+    @Test
+    public void testRetieveMaxAnagramSetFromFile() {
+        when(mockedDictionaryService.retrieveWordList()).thenReturn(
+                readWordsListFromFile("wordlist.txt"));
+
+        Anagramatron anagramatron = new AnagramatronImpl(mockedDictionaryService);
+
+        List<String> maxAnagramSet = anagramatron.retrieveMaxAnagramSet();
+        System.out.println(maxAnagramSet);
+        assertEquals(maxAnagramSet.size(), 14);
+    }
+
+}
